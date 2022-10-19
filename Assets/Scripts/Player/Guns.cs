f91a59f4127e5751d10dd2a1cfee38d985348b91
PowGrow <slideb910@gmail.com>
2022-10-19 09:00:32 +0300

    Added: Missile weapon type

--- Assets/Scripts/Player/Guns.cs
+++ Assets/Scripts/Player/Guns.cs
@@ -7,7 +7,7 @@ public class Guns : MonoBehaviour, IShooter
     [SerializeField] private List<Gun> _guns;
     private List<Transform> _gunMountPoints;
 
-    private float _timer;
+    private  List<float> _gunTimers;
     public void Fire()
     {
         if(_guns.Count == 1)
@@ -21,23 +21,30 @@ public class Guns : MonoBehaviour, IShooter
     {
         for (; startGunIndex < _guns.Count; startGunIndex++)
         {
-            if (_timer > _guns[startGunIndex].shootingInterval)
+            if (_gunTimers[startGunIndex] > _guns[startGunIndex].shootingInterval)
             {
                 var _bullet = Instantiate(_guns[startGunIndex].projectilePrefab) as GameObject;
                 _bullet.transform.position = _gunMountPoints[startGunIndex].position;
                 _bullet.transform.rotation = _gunMountPoints[startGunIndex].rotation;
+                _gunTimers[startGunIndex] = 0;
             }
         }
-        _timer = 0;
     }
 
     private void Update()
     {
-        _timer += Time.deltaTime;
+        for(int i = 0; i < _gunTimers.Count; i++)
+        {
+            _gunTimers[i] += Time.deltaTime;
+        }
     }
     private void Awake()
     {
-        _timer = 0;
+        _gunTimers = new List<float>();
+        for(int i = 0; i < _guns.Count; i++)
+        {
+            _gunTimers.Add(0);
+        }
         _gunMountPoints = GetComponentsInChildren<Transform>().ToList();
         _gunMountPoints.RemoveAt(0);
     }
